4a1a05c3ad1ac34a631ee1b3e963044bfb95a572
chapai <chepizhenko.alex@gmail.com>
2013-12-04 04:38:36 +0200

    Basic template views.

---
+++ application/views/light/core/head.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * @version 1.0
+ * @package default
+ * @author chapai <chepizhenko.alex@gmail.com>
+ * @date 29-10-2013
+ */
+
+?>
+
+<link rel="stylesheet" href="/css/normalize.css">
+<link rel="stylesheet" href="/css/foundation.css">
+
+<link rel="stylesheet" href="/css/styles.css">
+
+<script src="/js/vendor/custom.modernizr.js"></script>
---
+++ application/views/light/template.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *
+ * @version 1.0
+ * @package default
+ * @author chapai <chepizhenko.alex@gmail.com>
+ * @date 29-10-2013
+ */
+
+?>
+
+
+<!DOCTYPE html>
+<!--[if IE 9]>
+    <html class="lt-ie10" lang="en" >
+<![endif]-->
+
+<html class="no-js" lang="en" >
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Homework</title>
+
+        <?=@$head?>
+
+    </head>
+
+    <body>
+
+    <?=@$content?>
+
+    <script src="/js/vendor/jquery.js"></script>
+    <script src="/js/foundation.min.js"></script>
+
+    <script>
+        $(document).foundation();
+    </script>
+
+</body>
+</html>
