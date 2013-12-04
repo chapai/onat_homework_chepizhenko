d7621f0a5bb851fa811ead9b5e0aebce2bcc0d3e
chapai <chepizhenko.alex@gmail.com>
2013-12-04 04:50:51 +0200

    Proper prefix for base classes and some routes for articles.

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,9 +38,10 @@
 |
 */
 
-$route['default_controller'] = "welcome";
+$route['default_controller'] = "article";
 $route['404_override'] = '';
 
+$route["article/(:any)"] = "article/showSingleArticle/$1";
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
