c85d992771f72982506421ccb7251a617a3a2c06
chapai <chepizhenko.alex@gmail.com>
2013-12-06 20:21:03 +0200

    Routes for category CRUD (admin controller)

--- application/config/routes.php
+++ application/config/routes.php
@@ -48,5 +48,9 @@ $route["admin/article/delete/(:any)"] = "admin/deletearticle/$1";
 $route["admin/article/new"] = "admin/newarticle";
 $route["admin/article/list"] = "admin/listarticles";
 
+$route["admin/category/edit/(:any)"] = "admin/editcategory/$1";
+$route["admin/category/delete/(:any)"] = "admin/deletecategory/$1";
+$route["admin/category/new"] = "admin/newcategory";
+$route["admin/category/list"] = "admin/listcategories";
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
